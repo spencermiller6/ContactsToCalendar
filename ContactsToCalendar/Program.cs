7131bcc2bcf0878a641c8b40bf223926bd9067f8
Spencer Miller <spencer.miller6.0@gmail.com>
2023-11-07 18:00:57 -0800

    Created namespace, driver class, and main method

--- ContactsToCalendar/Program.cs
+++ ContactsToCalendar/Program.cs
@@ -1,166 +1,173 @@
-﻿using System.Globalization;
-using System.Reflection.Metadata;
-using System.Text;
+﻿using System.Text;
 
-Console.WriteLine("Program started");
-
-const string filepathIn = "C:\\Users\\spenc\\Downloads\\ContactsIn.vcf";
-const string filepathOut = "C:\\Users\\spenc\\Downloads\\CalendarOut.ics";
-
-StreamReader reader = new StreamReader(filepathIn);
-StreamWriter writer = new StreamWriter(filepathOut);
-
-Console.WriteLine("Files opened");
-
-List<Contact> contacts = Contact.ParseContactList(filepathIn);
-
-writer.WriteLine("BEGIN:VCALENDAR");
-writer.WriteLine("METHOD:PUBLISH");
-writer.WriteLine("PRODID:ContactsToCalendar v0.1");
-writer.WriteLine("VERSION:2.0");
-
-foreach (Contact contact in contacts)
+namespace ContactsToCalendar
 {
-    string exportedContact = contact.Export();
-    writer.Write(exportedContact);
-}
-
-writer.WriteLine("END:VCALENDAR");
-writer.Close();
-
-public class Contact
-{
-    private string? _birthday;
-    public string? Name;
-    public string? Birthday
+    class ContactsToCalendar
     {
-        get => _birthday;
-        set
+        static void Main(string[] args)
         {
-            string minDate = "2000";
+            Console.WriteLine("Program started");
 
-            if (Int32.Parse(value.Substring(0, 4)) < Int32.Parse(minDate))
+            const string filepathIn = "C:\\Users\\spenc\\Downloads\\ContactsIn.vcf";
+            const string filepathOut = "C:\\Users\\spenc\\Downloads\\CalendarOut.ics";
+
+            StreamReader reader = new StreamReader(filepathIn);
+            StreamWriter writer = new StreamWriter(filepathOut);
+
+            Console.WriteLine("Files opened");
+
+            List<Contact> contacts = Contact.ParseContactList(filepathIn);
+
+            writer.WriteLine("BEGIN:VCALENDAR");
+            writer.WriteLine("METHOD:PUBLISH");
+            writer.WriteLine("PRODID:ContactsToCalendar v0.1");
+            writer.WriteLine("VERSION:2.0");
+
+            foreach (Contact contact in contacts)
             {
-                StringBuilder sb = new StringBuilder(value);
-
-                for (int i = 0; i < 4; i++)
-                {
-                    sb[i] = minDate[i];
-                }
-
-                _birthday = sb.ToString();
+                string exportedContact = contact.Export();
+                writer.Write(exportedContact);
             }
-            else
-            {
-                _birthday = value;
-            }
+
+            writer.WriteLine("END:VCALENDAR");
+            writer.Close();
         }
     }
 
-    public static List<Contact> ParseContactList(string filepathIn)
+    public class Contact
     {
-        List<Contact> contacts = new List<Contact>();
-        StreamReader reader = new StreamReader(filepathIn);
-        string? lineIn;
-
-        try
+        private string? _birthday;
+        public string? Name;
+        public string? Birthday
         {
-            lineIn = reader.ReadLine();
-
-            while (lineIn != null)
+            get => _birthday;
+            set
             {
-                string property;
-                string[] values;
+                string minDate = "2000";
 
-                ParseLine(lineIn, out property, out values);
-
-                if (property == "BEGIN")
+                if (Int32.Parse(value.Substring(0, 4)) < Int32.Parse(minDate))
                 {
-                    Contact contact = ParseContact(ref reader);
+                    StringBuilder sb = new StringBuilder(value);
 
-                    if (!String.IsNullOrEmpty(contact.Birthday))
+                    for (int i = 0; i < 4; i++)
                     {
-                        contacts.Add(contact);
+                        sb[i] = minDate[i];
                     }
+
+                    _birthday = sb.ToString();
                 }
-
-                lineIn = reader.ReadLine();
-            }
-        }
-        finally
-        {
-            reader.Close();
-        }
-
-        return contacts;
-    }
-
-    public static void ParseLine(string line, out string property, out string[] values)
-    {
-        string[] substrings = line.Split(':', ';');
-
-        property = substrings[0];
-        values = new string[substrings.Length - 1];
-
-        for (int i = 1; i < substrings.Length; i++)
-        {
-            values[i - 1] = substrings[i];
-        }
-    }
-
-    public static Contact ParseContact(ref StreamReader reader)
-    {
-        Contact contact = new Contact();
-        string? lineIn = reader.ReadLine();
-
-        try
-        {
-            while (lineIn != null)
-            {
-                string property;
-                string[] values;
-
-                ParseLine(lineIn, out property, out values);
-
-                switch (property)
+                else
                 {
-                    case "FN":
-                        contact.Name = values[0];
-                        break;
-                    case "BDAY":
-                        contact.Birthday = values[1];
-                        break;
-                    case "END": return contact;
-                    default: break;
+                    _birthday = value;
                 }
-
-                lineIn = reader.ReadLine();
             }
         }
-        finally
+
+        public static List<Contact> ParseContactList(string filepathIn)
         {
+            List<Contact> contacts = new List<Contact>();
+            StreamReader reader = new StreamReader(filepathIn);
+            string? lineIn;
+
+            try
+            {
+                lineIn = reader.ReadLine();
+
+                while (lineIn != null)
+                {
+                    string property;
+                    string[] values;
+
+                    ParseLine(lineIn, out property, out values);
+
+                    if (property == "BEGIN")
+                    {
+                        Contact contact = ParseContact(ref reader);
+
+                        if (!String.IsNullOrEmpty(contact.Birthday))
+                        {
+                            contacts.Add(contact);
+                        }
+                    }
+
+                    lineIn = reader.ReadLine();
+                }
+            }
+            finally
+            {
+                reader.Close();
+            }
+
+            return contacts;
         }
 
-        return contact;
-    }
+        public static void ParseLine(string line, out string property, out string[] values)
+        {
+            string[] substrings = line.Split(':', ';');
 
-    public string Export()
-    {
+            property = substrings[0];
+            values = new string[substrings.Length - 1];
 
-        StringBuilder sb = new StringBuilder();
+            for (int i = 1; i < substrings.Length; i++)
+            {
+                values[i - 1] = substrings[i];
+            }
+        }
 
-        sb.AppendLine("BEGIN:VEVENT");
-        sb.AppendLine("Summary:" + Name + "'s Birthday");
-        sb.AppendLine("UID:" + Guid.NewGuid().ToString());
-        sb.AppendLine("SEQUENCE:0");
-        sb.AppendLine("STATUS:CONFIRMED");
-        sb.AppendLine("TRANSP:TRANSPARENT");
-        sb.AppendLine("RRULE:FREQ=YEARLY");
-        sb.AppendLine("DTSTART;VALUE=DATE:" + Birthday.Replace("-", ""));
-        sb.AppendLine("DTEND;VALUE=DATE:" + Birthday.Replace("-", ""));
-        sb.AppendLine("DTSTAMP:" + DateTime.UtcNow.ToString("yyyyMMdd") + "T000000");
-        sb.AppendLine("END:VEVENT");
+        public static Contact ParseContact(ref StreamReader reader)
+        {
+            Contact contact = new Contact();
+            string? lineIn = reader.ReadLine();
 
-        return sb.ToString();
+            try
+            {
+                while (lineIn != null)
+                {
+                    string property;
+                    string[] values;
+
+                    ParseLine(lineIn, out property, out values);
+
+                    switch (property)
+                    {
+                        case "FN":
+                            contact.Name = values[0];
+                            break;
+                        case "BDAY":
+                            contact.Birthday = values[1];
+                            break;
+                        case "END": return contact;
+                        default: break;
+                    }
+
+                    lineIn = reader.ReadLine();
+                }
+            }
+            finally
+            {
+            }
+
+            return contact;
+        }
+
+        public string Export()
+        {
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("BEGIN:VEVENT");
+            sb.AppendLine("Summary:" + Name + "'s Birthday");
+            sb.AppendLine("UID:" + Guid.NewGuid().ToString());
+            sb.AppendLine("SEQUENCE:0");
+            sb.AppendLine("STATUS:CONFIRMED");
+            sb.AppendLine("TRANSP:TRANSPARENT");
+            sb.AppendLine("RRULE:FREQ=YEARLY");
+            sb.AppendLine("DTSTART;VALUE=DATE:" + Birthday.Replace("-", ""));
+            sb.AppendLine("DTEND;VALUE=DATE:" + Birthday.Replace("-", ""));
+            sb.AppendLine("DTSTAMP:" + DateTime.UtcNow.ToString("yyyyMMdd") + "T000000");
+            sb.AppendLine("END:VEVENT");
+
+            return sb.ToString();
+        }
     }
 }
